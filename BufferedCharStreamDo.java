06e188e08c6f905a85d9663e9eafb3f4a44383b9
ChangPulMu <16906@naver.com>
2019-04-11 09:36:21 +0900

    BufferedCharStream 구현 파일

---
+++ BufferedCharStreamDo.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+public class BufferedCharStreamDo
+{
+	public static void main(String[] argv)
+	{
+		int i, len = 0;
+		String crSet = "_UTF-8";
+		
+		String sourFile = "./bin/data/base"+crSet+".txt";
+		String destFile = "./bin/data/output_char"+crSet+".txt";
+		
+		System.out.println("Source Name : "+sourFile);
+		System.out.println("Target Name : "+destFile);
+		
+		try
+		{
+			BufferedReader br = null;
+			BufferedWriter bw = null;
+			
+			br = new BufferedReader(new FileReader(sourFile));
+			bw = new BufferedWriter(new FileWriter(destFile));
+			
+			while((i=br.read())!=-1)
+			{
+				bw.write(i);
+				
+				len+=1;
+				System.out.println("Read data["+i+","+len+"]");
+			}
+			br.close();
+			bw.close();
+			System.out.println("finished");
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
