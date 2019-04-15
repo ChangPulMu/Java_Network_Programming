a85373e82fa9213c4847b127d2de030ddf4d781f
ChangPulMu <16906@naver.com>
2019-04-15 22:26:07 +0900

    InputStream 구현 파일

---
+++ InputStreamDo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class InputStreamDo{
+	public static void main(String[] args) {
+		InputStream in = System.in;
+		
+		try {
+			while(true) {
+				int i = in.read();
+				
+				if(i==-1)
+					break;
+				char c = (char)i;
+				
+				System.out.print(c);
+			}
+		}
+		catch(IOException ie)
+		{
+			System.out.println("예외냐? 이것때문 : "+ie);
+		}
+	}
+}
